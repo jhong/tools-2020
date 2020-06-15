c57a7ead18e9c776585ef6963f02868a29a65ff9
jhong <jhsign@gmail.com>
2020-06-15 17:00:05 +0900

    hibernate sample 작성

---
+++ src/main/java/tools/cmm/DateUtil.java
@@ -0,0 +1,100 @@
+package tools.cmm;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class DateUtil {
+//	static Logger logger = LoggerFactory.getLogger(DateUtil.class);
+
+	public final static String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	
+	/**
+	 * 현재 시간(Date) 반환
+	 * @return
+	 * @throws Exception
+	 */
+	public static Date getCurrentDate() throws Exception {
+		Calendar cal = Calendar.getInstance();
+		return cal.getTime();
+	}
+	
+	/**
+	 * 현재 시간 반환 (자동생성 파일명 뒤에 사용하기 위한 포맷)
+	 * @return
+	 * @throws Exception
+	 */
+	public static String getCurrTimePostfixStr() throws Exception {
+		SimpleDateFormat formatter = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		String today = null;
+		today = formatter.format(cal.getTime());
+		Timestamp ts = Timestamp.valueOf(today);
+		return ts.toString().replaceAll("-", "").replaceAll(" ", "").replaceAll(":", "");
+	}
+
+	/**
+	 * 현재 시간을 포매팅하여 반환한다.
+	 * @param dtFormat
+	 * @return 날짜 스트링
+	 */
+	public static String getNowString(String dtFormat) {
+		Calendar today = Calendar.getInstance();
+		String szResult = "";
+		if (dtFormat != null) {
+//			szResult = new SimpleDateFormat(dtFormat).format(today.getTime());
+			szResult = new SimpleDateFormat(dtFormat, Locale.ENGLISH).format(today.getTime());
+		}
+		return szResult;
+	}
+	
+//	public static Calendar str2cal(String str, String format) {
+//		return str2cal(str, format, Locale.KOREA);
+//	}
+//	
+//	public static Calendar str2cal(String str, String format, Locale locale) {
+//		Calendar cal = null;
+//		if (StringUtils.isEmpty(str)) return cal;
+//		
+//		try {
+////			DateFormat formatter = new SimpleDateFormat(format);
+//			DateFormat formatter = new SimpleDateFormat(format, locale);
+//			Date date = (Date)formatter.parse(str); 
+//			
+//			cal = Calendar.getInstance();
+//			cal.setTime(date);
+//		} catch (NullPointerException e) {
+//			logger.error("fail in str2cal() str="+str+", format="+format, e);
+//		} catch (IllegalArgumentException e) {
+//			logger.error("fail in str2cal() str="+str+", format="+format, e);
+//		} catch (Exception e) {
+//			logger.error("fail in str2cal() str="+str+", format="+format, e);
+//		}
+//		return cal;
+//	}
+	
+	public static String date2str(Date date, String format) {
+		if (date == null || StringUtils.isEmpty(format)) return null;
+		return new SimpleDateFormat(format).format(date);
+	}
+	
+	 /**
+	  * 표준포멧에 의한 날짜 형식을 가져온다.
+	  * @param dt
+	  * @param dtFormat
+	  * @return 날짜 스트링
+	  */
+	 public static String getDateString(java.util.Date dt, String dtFormat) {
+	     String szResult = "";
+	     if (dt != null && dtFormat != null) {
+	    	 szResult = new SimpleDateFormat(dtFormat).format(dt);
+
+	     }
+	     return szResult;
+	 }
+	
+}
---
+++ src/main/java/tools/data/DataInfo.java
@@ -0,0 +1,120 @@
+package tools.data;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import tools.cmm.Util;
+
+@Entity(name="ZZ_DATA")
+public class DataInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "title")	private String title;
+	@Column(name = "url")	private String url;
+	@Column(name = "data_type")	private String dataType;
+	@Column(name = "source")	private String source;
+	@Column(name = "content")	private String content;
+	@Column(name = "hit_count")	private Integer hitCount;
+	@Temporal(TemporalType.TIMESTAMP) @Column(name = "pub_date")	private Date pubDate;
+	@Temporal(TemporalType.TIMESTAMP) @Column(name = "col_date")	private Date colDate;
+	@Column(name = "use_yn")	private String useYn;
+
+	/**
+	 * toString()
+	 * @return String
+	 */
+    public String toString() {
+    	return Util.reflectionToString(this);
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDataType() {
+		return dataType;
+	}
+
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getHitCount() {
+		return hitCount;
+	}
+
+	public void setHitCount(Integer hitCount) {
+		this.hitCount = hitCount;
+	}
+
+	public Date getPubDate() {
+		return pubDate;
+	}
+
+	public void setPubDate(Date pubDate) {
+		this.pubDate = pubDate;
+	}
+
+	public Date getColDate() {
+		return colDate;
+	}
+
+	public void setColDate(Date colDate) {
+		this.colDate = colDate;
+	}
+
+	public String getUseYn() {
+		return useYn;
+	}
+
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
+    
+}
